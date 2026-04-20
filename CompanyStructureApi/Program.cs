161f0841f6b419e7f83020019babddbcfc9d8ff8
Richard Vymazal <richard.vymazal@gmail.com>
2026-04-20 11:48:27 +0200

    Added ScalaR to project

--- CompanyStructureApi/Program.cs
+++ CompanyStructureApi/Program.cs
@@ -1,5 +1,4 @@
-using CompanyStructureApi.Data;
-using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +13,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
